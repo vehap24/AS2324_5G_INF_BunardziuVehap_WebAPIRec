cb0722693f45fa29ba34677f61867f5ffbfab353
vehap24 <bvehap46@gmail.com>
2024-05-15 08:36:47 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/OperazioniController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_BunardziuVehap_WebAPIRec.Controllers
+{
+    public class OperazioniController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}

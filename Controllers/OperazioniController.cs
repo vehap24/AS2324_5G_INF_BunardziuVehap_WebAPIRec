c40126b311d6e1ab8439c7046cf01ff9009fd751
vehap24 <bvehap46@gmail.com>
2024-05-15 08:42:40 +0200

    feat: Aggiunto il metodo CelsiusToFahrenheit al nostro controller

--- Controllers/OperazioniController.cs
+++ Controllers/OperazioniController.cs
@@ -8,6 +8,10 @@ namespace AS2324_5G_INF_BunardziuVehap_WebAPIRec.Controllers
         {
             return View();
         }
-        
+        [HttpGet("CelsiusToFahrenheit")]
+        public JsonResult CelsiusToFahrenheit(float A)
+        {
+            return Json(new { message = "Conversione fatta", status = "OK", output = (A*1.8)+32 });
+        }
     }
 }

a641e44f40a48a35e1aaf1b1f58c63a38f0680c0
vehap24 <bvehap46@gmail.com>
2024-05-15 08:50:40 +0200

    feat: Aggiunta funzione CylinderVolume

--- Controllers/OperazioniController.cs
+++ Controllers/OperazioniController.cs
@@ -13,5 +13,18 @@ namespace AS2324_5G_INF_BunardziuVehap_WebAPIRec.Controllers
         {
             return Json(new { message = "Conversione fatta", status = "OK", output = (A*1.8)+32 });
         }
+        [HttpGet("CylinderVolume")]
+        public JsonResult CylinderVolume(float H, float R)
+        {
+            if(H > 0 &&  R > 0)
+            {
+                return Json(new { message = "Calcolo eseguito", status = "OK", output = (((R*R) * 3.14) * H) });
+            }
+            else
+            {
+                return Json(new { message = "Valore negativo inserito", status = "KO", output = 0 });
+            }
+            
+        }
     }
 }
